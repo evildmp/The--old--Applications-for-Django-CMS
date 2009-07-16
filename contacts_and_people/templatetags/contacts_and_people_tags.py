034c31092761a5ecf93875976a779588dad712eb
daniele <daniele@w128.medcn.uwcm.ac.uk>
2009-07-16 12:47:06 +0100

    Documentation

--- contacts_and_people/templatetags/contacts_and_people_tags.py
+++ contacts_and_people/templatetags/contacts_and_people_tags.py
@@ -7,10 +7,19 @@ from contacts_and_people.models import *
 register = template.Library()    
 
 @register.inclusion_tag('entitytree.html', takes_context=True)
-def make_membership_tree(context, node):
+def make_membership_tree(context, person, node):
+    """This function recurses, by using the template entitytree.html which in turn calls this function """
+    if node in person.gather_entities():
+        return {
+            'node': node,
+            'person': person,
+            }
+            
+@register.inclusion_tag('entitytree.html', takes_context=True)
+def list_members_of_entity(context, node):
     person=context['person']
     if node in person.gather_entities():
         return {
             'node': node,
             'person': person,
-            }                       
+            }    
