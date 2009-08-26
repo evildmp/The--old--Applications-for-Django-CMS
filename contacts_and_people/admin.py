dceaf55a86127aa7107bafb1c82af3133019bf8d
stefanfoulis <stefan.foulis@gmail.com>
2009-08-27 00:47:33 +0800

    added support for creating users without passwords in the admin added inline person edit in user view
    Signed-off-by: evildmp <daniele@apple-juice.co.uk>

--- contacts_and_people/admin.py
+++ contacts_and_people/admin.py
@@ -161,3 +161,155 @@ admin.site.register(models.Entity,EntityAdmin)
 admin.site.register(models.Site,SiteAdmin)
 admin.site.register(models.Title,TitleAdmin)
 admin.site.register(models.Membership,MembershipAdmin)
+
+
+
+
+
+# admin hacks
+use_admin_hacks = True
+if use_admin_hacks:
+    admin.site.unregister(User)
+    
+    from django import template
+    from django.conf import settings
+    from django.contrib import admin
+    from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AdminPasswordChangeForm
+    from django.contrib.auth.models import User, Group
+    from django.core.exceptions import PermissionDenied
+    from django.http import HttpResponseRedirect, Http404
+    from django.shortcuts import render_to_response, get_object_or_404
+    from django.template import RequestContext
+    from django.utils.html import escape
+    from django.utils.translation import ugettext, ugettext_lazy as _
+    
+    from django.contrib.auth.admin import UserAdmin
+    from django.contrib.auth.forms import UserCreationForm, UserChangeForm, AdminPasswordChangeForm
+    from django import forms
+    
+    class MyPersonInline(admin.StackedInline):
+        model = models.Person
+        fieldsets = PersonAdmin.fieldsets
+        prepopulated_fields = PersonAdmin.prepopulated_fields
+        extra = 1
+        verbose_name = ""
+        verbose_name_plural = "Person"
+    
+    
+    
+    class MyNoPasswordCapableUserCreationForm(UserCreationForm):
+        has_password = forms.BooleanField(required=False, initial=True)
+        def __init__(self, *args, **kwargs):
+            r = super(MyNoPasswordCapableUserCreationForm,self).__init__(*args, **kwargs)
+            instance = kwargs.get('instance',None)
+            if instance:
+                if instance.has_usable_password():
+                    self.initial['has_password'] = True
+                else:
+                    self.initial['has_password'] = False     
+            return r
+        def clean(self):
+            data = self.cleaned_data
+            if self.cleaned_data['has_password'] in (False, None,):
+                if 'password1' in self.errors.keys():
+                    del self.errors['password1']
+                if 'password2' in self.errors.keys():
+                    del self.errors['password2']
+                # save() will remove this temp password again.
+                self.cleaned_data['password1'] = self.cleaned_data['password2'] = 'xxxxxxxxxxxxxxx'
+            return data
+        def save(self, commit=True):
+            instance = super(MyNoPasswordCapableUserCreationForm, self).save(commit=False)
+            if self.cleaned_data['has_password'] in (False, None,):
+                instance.set_unusable_password()
+            if commit:
+                instance.save()
+                if hasattr(instance, 'save_m2m'):
+                    instance.save_m2m()
+                return instance
+            else:
+                return instance
+        
+    class MyNoPasswordCapableUserChangeForm(UserChangeForm):
+        has_password = forms.BooleanField(label="has password", help_text="LDAP users don't need a password", required=False, initial=True)
+        def __init__(self, *args, **kwargs):
+            r = super(MyNoPasswordCapableUserChangeForm,self).__init__(*args, **kwargs)
+            instance = kwargs.get('instance',None)
+            if instance:
+                if instance.has_usable_password():
+                    self.initial['has_password'] = True
+                else:
+                    self.initial['has_password'] = False     
+            return r
+        def save(self, commit=True):
+            instance = super(MyNoPasswordCapableUserChangeForm, self).save(commit=False)
+            if self.cleaned_data['has_password'] in (False, None,):
+                instance.set_unusable_password()
+            if commit:
+                instance.save()
+                instance.save_m2m()
+                return instance
+            else:
+                return instance
+    
+    user_admin_fieldsets = list(UserAdmin.fieldsets)
+    user_admin_fieldsets[0] = (None, {'fields': ('username', ('password', 'has_password',),)})
+    class MyUserAdmin(UserAdmin):
+        fieldsets = user_admin_fieldsets
+        form = MyNoPasswordCapableUserChangeForm
+        add_form = MyNoPasswordCapableUserCreationForm
+        list_display = ('username', 'email', 'is_staff')
+        inlines = (MyPersonInline,)
+        
+        def add_view(self, request):
+            # IT REALLY SUCKS THAT I NEED TO COPY THIS ENTIRE METHOD... it's the same as 
+            # django.contrib.auth.admin.UserAdmin.add_view, just with a other template
+            
+            # It's an error for a user to have add permission but NOT change
+            # permission for users. If we allowed such users to add users, they
+            # could create superusers, which would mean they would essentially have
+            # the permission to change users. To avoid the problem entirely, we
+            # disallow users from adding users if they don't have change
+            # permission.
+            if not self.has_change_permission(request):
+                if self.has_add_permission(request) and settings.DEBUG:
+                    # Raise Http404 in debug mode so that the user gets a helpful
+                    # error message.
+                    raise Http404('Your user does not have the "Change user" permission. In order to add users, Django requires that your user account have both the "Add user" and "Change user" permissions set.')
+                raise PermissionDenied
+            if request.method == 'POST':
+                form = self.add_form(request.POST)
+                if form.is_valid():
+                    new_user = form.save()
+                    msg = _('The %(name)s "%(obj)s" was added successfully.') % {'name': 'user', 'obj': new_user}
+                    self.log_addition(request, new_user)
+                    if "_addanother" in request.POST:
+                        request.user.message_set.create(message=msg)
+                        return HttpResponseRedirect(request.path)
+                    elif '_popup' in request.REQUEST:
+                        return self.response_add(request, new_user)
+                    else:
+                        request.user.message_set.create(message=msg + ' ' + ugettext("You may edit it again below."))
+                        return HttpResponseRedirect('../%s/' % new_user.id)
+            else:
+                form = self.add_form()
+            return render_to_response('contacts_and_people/admin/auth/user/add_form.html', {
+                'title': _('Add user'),
+                'form': form,
+                'is_popup': '_popup' in request.REQUEST,
+                'add': True,
+                'change': False,
+                'has_add_permission': True,
+                'has_delete_permission': False,
+                'has_change_permission': True,
+                'has_file_field': False,
+                'has_absolute_url': False,
+                'auto_populated_fields': (),
+                'opts': self.model._meta,
+                'save_as': False,
+                'username_help_text': self.model._meta.get_field('username').help_text,
+                'root_path': self.admin_site.root_path,
+                'app_label': self.model._meta.app_label,            
+            }, context_instance=template.RequestContext(request))
+    admin.site.register(User, MyUserAdmin)
+
